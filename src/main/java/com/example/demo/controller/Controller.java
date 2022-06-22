e56db04ffe4076043096b8b8ccb55a9e489cb0a0
Md-ObyduRahaman <sojib.19991018@gmail.com>
2022-06-22 13:05:59 +0600

    first commit

---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+	
+	@RequestMapping("/")
+	public String censusForm() {
+		System.out.println("hello CensusForm");
+		
+		return "CensusEntryForm";
+	}
+	
+	
+
+}
