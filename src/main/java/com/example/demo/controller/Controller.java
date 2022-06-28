6067a6e8ba28350016e637adc52b3f522c11ba67
Md-ObyduRahaman <sojib.19991018@gmail.com>
2022-06-28 17:17:35 +0600

    3 page done

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @org.springframework.stereotype.Controller
 public class Controller {
 	
-	@RequestMapping("/")
+	@RequestMapping("/CensusEntryForm")
 	public String censusForm() {
 		
 		
